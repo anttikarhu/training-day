3cb17228eb37d9928bfef908cae8e5578e1a8cde
anttikar <antti.karhu@solita.fi>
2017-05-09 19:39:38 +0300

    Doniz

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,32 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
+
+
+
+
+
+
+
+
+
+; Excercise 1
+(+ 4 (* 2 3))
+
+; Excercise 2
+(+ 3 4 5 6)
+
+; Excercise 3
+(get "abrakadabra" 0)
+
+; Excercise 4
+((fn [name] (str "Welcome to Rivendell mr " name)) "Shaggins")
